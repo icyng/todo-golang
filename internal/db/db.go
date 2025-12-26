83d10a2bef42071082e0ef0b722d84e364785db2
icyng <take523oliver@gmail.com>
2025-12-27 05:38:28 +0900

    add: db connect

---
+++ internal/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func New(dsn string) (*gorm.DB, error) {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             200 * time.Millisecond,
+			LogLevel:                  logger.Warn,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  false,
+		},
+	)
+
+	return gorm.Open(sqlite.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+}
