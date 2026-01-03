742daa3212fc6bf42b27ea062b39c700c9223e38
icyng <take523oliver@gmail.com>
2026-01-03 12:41:29 +0900

    update: 優先度と期限

--- internal/domain/todo.go
+++ internal/domain/todo.go
@@ -3,9 +3,11 @@ package domain
 import "time"
 
 type Todo struct {
-	ID        uint      `json:"id"`
-	Title     string    `json:"title"`
-	Done      bool      `json:"done"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint       `json:"id"`
+	Title     string     `json:"title"`
+	Done      bool       `json:"done"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	Priority  uint8      `json:"priority" gorm:"not null;default:3"`
+	DueAt     *time.Time `json:"due_at"`
 }
