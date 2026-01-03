9d9248cf2ae67d8e4d80866ab011cbe2b5889db8
icyng <take523oliver@gmail.com>
2026-01-03 12:42:04 +0900

    update: option追加

--- internal/rest/dto.go
+++ internal/rest/dto.go
@@ -1,10 +1,40 @@
 package rest
 
-type createTodoRequest struct {
-	Title string `json:"title" binding:"required"`
-}
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+)
 
 type updateTodoRequest struct {
-	Title string `json:"title" binding:"required"`
-	Done  *bool  `json:"done" binding:"required"`
+	Title    string       `json:"title" binding:"required"`
+	Done     *bool        `json:"done" binding:"required"`
+	Priority *uint8       `json:"priority" binding:"omitempty,min=1,max=5"`
+	DueAt    OptionalTime `json:"due_at"`
+}
+
+type createTodoRequest struct {
+	Title    string     `json:"title" binding:"required"`
+	Priority *uint8     `json:"priority" binding:"omitempty,min=1,max=5"`
+	DueAt    *time.Time `json:"due_at"`
+}
+
+type OptionalTime struct {
+	Time *time.Time
+	Set  bool
+}
+
+func (o *OptionalTime) UnmarshalJSON(b []byte) error {
+	o.Set = true
+	if bytes.Equal(bytes.TrimSpace(b), []byte("null")) {
+		o.Time = nil
+		return nil
+	}
+
+	var t time.Time
+	if err := json.Unmarshal(b, &t); err != nil {
+		return err
+	}
+	o.Time = &t
+	return nil
 }
